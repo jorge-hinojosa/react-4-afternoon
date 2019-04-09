15e699bd27da3196408ebb8a5b2757d1560b4d4b
Jorge Hinojosa <jorgexhinojosa@gmail.com>
2019-04-09 15:23:58 -0500

    Finished React 4

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,18 +1,47 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default class ClassList extends Component {
-  constructor() {
-    super()
-    
+  constructor(props) {
+    super(props);
+    console.log(props);
+    this.state = {
+      students: []
+    };
   }
-
+  componentDidMount() {
+    const course = `http://localhost:3005/students?class=${
+      this.props.match.params.class
+    }`;
+    axios.get(course).then(res => {
+      console.log(res);
+      return this.setState({ students: res.data });
+    });
+  }
   render() {
+    const { students } = this.state;
+    const course = this.props.match.params.class;
+
+    const viewStudents = students.map((student, i) => {
+      return (
+        <Link to={`/student/${student.id}`} key={i}>
+          <h3>
+            {student.first_name} {student.last_name}
+          </h3>
+        </Link>
+      );
+    });
+
     return (
       <div className="box">
-        <h1></h1>
+        <h1>{course}</h1>
         <h2>ClassList:</h2>
-
+        {viewStudents}
+        <button>
+          <Link to="/">Back to Home Page</Link>
+        </button>
       </div>
-    )
+    );
   }
 }
--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,16 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
+// import ClassList from "../ClassList/ClassList";
 
 export default class Student extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props);
+    this.state = {
+      studentInfo: {}
+    };
+  }
+  componentDidMount() {
+    const studentById = `http://localhost:3005/students/${
+      this.props.match.params.id
+    }`;
 
+    axios.get(studentById).then(res => {
+      console.log(res);
+      return this.setState({ studentInfo: res.data });
+    });
   }
 
   render() {
+    const { studentInfo } = this.state;
+
     return (
       <div className="box">
-        <h1>Student</h1>
+        <h1>Student: </h1>
+        <h1>
+          {studentInfo.first_name} {studentInfo.last_name}
+        </h1>
+        <h3>Email: {studentInfo.email}</h3>
+        <h3>Grade: {studentInfo.grade}</h3>
+        <button>
+          <Link to={`/classlist/${studentInfo.class}`}>Back to Class List</Link>
+        </button>
       </div>
-    )
+    );
   }
 }
